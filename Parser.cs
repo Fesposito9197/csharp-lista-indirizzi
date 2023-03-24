ccb96eb94d2de8c6c3a02949188cc26899ec4053
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-24 14:44:55 +0100

    aggiunge classe indirizzi e classe parser

---
+++ Parser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public static class Parser
+    {
+        public static IEnumerable<Indirizzo> Read()
+        {
+           using var input = File.OpenText("addresses.csv");
+
+            input.ReadLine();
+
+
+        }
+
+        public static void Write( IEnumerable<Indirizzo> indirizzi) 
+        {
+            using var output = File.CreateText("addresses.csv");
+        }
+
+    }
+}
---
+++ indirizzo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public record class Indirizzo
+    {
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zip = zip;
+        }
+
+        public string name { get; init; }
+        public string surname { get; init; }
+        public string street { get; init; }
+        public string city { get; init; }
+        public string province { get; init; }
+        public string zip { get; init; }
+    }
+}
