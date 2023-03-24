eb82c218d23e039bb5299175a0e7aa27ee3359d1
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-24 15:32:18 +0100

    aggiunge esercizio completato

--- Parser.cs
+++ Parser.cs
@@ -10,9 +10,31 @@ namespace csharp_lista_indirizzi
     {
         public static IEnumerable<Indirizzo> Read()
         {
-           using var input = File.OpenText("addresses.csv");
+           using var input = File.OpenText("..\\..\\..\\addresses.csv");
+           var indirizzi = new List<Indirizzo>();
 
-            input.ReadLine();
+           input.ReadLine();
+
+
+           while (true)
+           {
+                string? line = input.ReadLine();
+                if (line is null) return indirizzi;
+
+                var chunks = line.Split(',')!;
+                if (chunks.Length < 6) continue;
+                var name = chunks[0];
+                var surname = chunks[1];
+                var street = chunks[2];
+                var city = chunks[3];
+                var province = chunks[4];
+                var zip = chunks[5];
+                 
+                Indirizzo indirizzo = new Indirizzo(name , surname, street, city , province , zip);
+
+                indirizzi.Add(indirizzo);
+                
+            }
 
 
         }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_lista_indirizzi;
+
+var indirizzi = Parser.Read();
+var ultimiTreIndirizzi = indirizzi.Skip(Math.Max(0, indirizzi.Count() - 3)).Take(3);
+
+foreach (var indirizzo in indirizzi)
+{
+    Console.WriteLine(indirizzo);
+}
