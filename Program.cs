6661da29f1002caa1aacc6bff7a9095211b22504
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-24 15:33:07 +0100

    refactoring

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using csharp_lista_indirizzi;
 
 var indirizzi = Parser.Read();
-var ultimiTreIndirizzi = indirizzi.Skip(Math.Max(0, indirizzi.Count() - 3)).Take(3);
+
 
 foreach (var indirizzo in indirizzi)
 {
